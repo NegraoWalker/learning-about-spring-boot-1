94fc6e7f11399826ee61d10090232d4029b5f94d
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-04-21 15:02:05 -0300

    Repositório criado

---
+++ src/main/java/com/walker/learningaboutspringboot1/LearningAboutSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package com.walker.learningaboutspringboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningAboutSpringBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningAboutSpringBoot1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/walker/learningaboutspringboot1/LearningAboutSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.learningaboutspringboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningAboutSpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
